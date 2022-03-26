42cb8d7d3ede2a8aef14e6a3a41ff64a43e4e202
Alimbaeva-Burulai <84013664+Alimbaeva-Burulai@users.noreply.github.com>
2022-03-26 17:48:52 +0100

    Add files via upload

---
+++ Tax_calculation/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            txtCurrnet.Text += (sender as Button).Content.ToString();
+        }
+
+        private void DecSepClick(object sender, RoutedEventArgs e)
+        {
+            string decSep = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            if (!txtCurrnet.Text.Contains(decSep))
+            {
+                txtCurrnet.Text += decSep;
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            lblOper.Content = (sender as Button).Content.ToString();
+            lblOp1.Content = txtCurrnet.Text;
+            txtCurrnet.Text = "0";
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            lblOp2.Content = txtCurrnet.Text;
+
+            try
+            {
+                double op1 = double.Parse(lblOp1.Content.ToString());
+                double op2 = double.Parse(lblOp2.Content.ToString());
+                switch (lblOper.Content)
+                {
+                    case "+": lblResult.Content = op1 + op2; break;
+                    case "-": lblResult.Content = op1 - op2; break;
+                    case "*": lblResult.Content = op1 * op2; break;
+                    case "/": lblResult.Content = op1 / op2; break;
+                    
+                }
+                txtCurrnet.Text = lblResult.Content.ToString();
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Convert error");
+            }
+            catch(OverflowException)
+            {
+                MessageBox.Show("too big number");
+            }
+        }
+
+        private void txtCurrnet_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // e.Handled = !char.IsDigit(e.Text[0]);
+        }
+
+        private void txtCurrnet_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = (e.Key < Key.D0 || e.Key > Key.D9) && (e.Key < Key.NumPad0 || e.Key > Key.NumPad9);
+        }
+    }
+}
---
+++ Tax_calculation/Tax_calculation/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tax_calculation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Calculate(object sender, RoutedEventArgs e)
+        {
+            string s = string.Empty;
+            try
+            {
+                double income = double.Parse(txtIncome.Text);
+                double tax = double.Parse(txttaxPercent.Text);
+                double prepaid = double.Parse(txtPrepaid.Text);
+                double result = income * tax / 100 - prepaid;
+                if (result > 0)
+                {
+                    s = "Must pay more!" + result;
+                }
+                else if (result < 0)
+                {
+                    s = "Overpaid!" + (-result);
+                }
+                else
+                    s = "All is okay!";
+
+                MessageBox.Show(s);
+                lblResult.Content = s;
+            }catch(FormatException)
+            {
+                MessageBox.Show("Not a number!");
+            }
+            catch(OverflowException)
+            {
+                MessageBox.Show("Too big number");
+            }
+        }
+    }
+}
